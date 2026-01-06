0079e4392b4134ad5b5eb88deb52ea61862f33ef
Mann275 <patelmann2705@gmail.com>
2026-01-06 12:18:12 +0530

    js 6/1  control flow statements

--- JS/Basics.js
+++ JS/Basics.js
@@ -43,7 +43,7 @@ const t=100; //Declaration and inialization
 //const t=300; //Re-declaration is not valid , shows error
      
 // Temporal Dead Zone (TDZ)
-console.log(tdza); //TDZ , shows error
+// console.log(tdza); //TDZ , shows error
 let tdza=10;    // TDZ means the variable is in inaccessible state before initialization // like 1 to 45 tdza  is for tdza variable 
      
 // Hoisting Impact per variable type
@@ -54,9 +54,67 @@ var hoist=undefined; //Declaration is moved to the top
 hoist=1; //initialization remains in place
      
 // Data Types + Type system
-// Primitive Data types : number , string , boolean , null , undefined , symbol , bigint
+// Primitive Data types : 5ber , string , boolean , null , undefined , symbol , bigint
 // cannot store multiple values and its like duplicate the data each time when we put it in new variable 
 // Non-Primitive/Reference Data types : object {} , array [] , function ()
 // can store multiple values and its like store the address of the data in new variable
+//prompt is used to take input from user
  
 // Dynamic Typing : variable can hold any type of data at different times 
+//typeof operator : to know the data type of variable
+// Quirks = unexpected behavior
+ 
+//Type Coercion : automatic or implicit conversion of values from one data type to another
+//Explicit conversion : manual conversion of values from one data type to another using functions like String(), 5ber(), Boolean()
+//Implicit conversion : automatic conversion done by JavaScript during operations
+let tc1= '5' + 10; // '510' , 5ber 10 is coerced to string '10' , implicit conversion
+ 
+// Truthy and Falsy values 
+// Falsy values : false , 0 , '' (empty string) , null , undefined , NaN
+// Everything else is Truthy value
+//  == operator (loose equality) : compares values after type coercion
+//  === operator (strict equality) : compares values without type coercion , also checks data type 
+     
+//Operators
+/* 
+    Arithmetic Operators : + , - , * , / , % , ++ , --,**
+    Comparison Operators : == , === , != , !== , > , < , >= , <=
+    Assignment Operators : = , += , -= , *= , /= , %=
+    Logical Operators : && , || , !
+    Unary Operators : + , - , ++ , -- , ! , Typeof
+    Ternary Operator : condition ? expr1 : expr2
+    instanceof operator : checks if an object is an instance of a specific class or constructor function and works with non-primitive data types only
+*/
+     
+// Control Flow Statements
+// Conditional Statements : if , else , else-if , if-else if-else , switch-case , early return
+// Looping Statements : for , while , do-while , for...in , for...of , foreach
+// Jump Statements : break , continue , return
+ 
+/* 
+for loop : iterates a block of code a specified 5ber of times
+for(start;end;change){}
+while loop : iterates a block of code while a specified condition is true
+start
+while(end){change}
+do-while loop : iterates a block of code once, then repeats while a specified condition is true 
+do{change}
+while(end);
+//for loop
+for(let i=0 ; i<=5 ; i++){
+    console.log(i);
+}
+//while loop
+let i = 1;
+while(i<28){
+    console.log(i);
+    i++;
+}
+//do-while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<5);
+*/
+
