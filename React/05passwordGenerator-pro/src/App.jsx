ba3f92a959eae0669b97430da850b238b03442ff
Mann275 <patelmann2705@gmail.com>
2026-01-27 14:18:40 +0530

    react jan 27 other hooks useeffect,usedif , usecallback

---
+++ React/05passwordGenerator-pro/src/App.jsx
@@ -0,0 +1,97 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setnumAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useref Hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$-_&";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 99); // for optimization
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numAllowed, charAllowed, passwordGenerator]);
+
+  return (
+    <>
+      <div className="w=full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-2 text-black bg-gray-800 ">
+        <h1 className="text-1xl text-center text-white my-2">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bg-white"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="bg-black text-white px-3 py-2 shrink-0 outline-none cursor-copy"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2 text-white">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={27}
+              value={length}
+              className="cursor-grab"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label className="text-white">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id="numInput"
+              onChange={() => {
+                setnumAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numInput">Numbers</label>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="CharInput"
+              onChange={() => {
+                setcharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="CharInput">Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
